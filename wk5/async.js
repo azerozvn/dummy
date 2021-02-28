472f600acc2cfbd4a30b2d9bc143b08e484126f1
Azer <nguoichet001@yahoo.com>
2021-02-27 21:56:42 -0800

    Add wk5

---
+++ wk5/async.js
@@ -0,0 +1,24 @@
+// console.log('1st');
+// setTimeout(() => { console.log('2nd'); }, 0);
+// Promise.resolve().then(()=> { console.log('3rd'); });
+// console.log('4th');
+
+// Promise.resolve()
+// .then(()=> {
+// console.log('1st');
+// })
+// .then(()=> {
+// console.log('2nd');
+// }).then(()=> {
+// console.log('3rd');
+// }).then(()=> {
+// console.log('4th');
+// });
+
+// const numbers = ['1st', '2nd', '3rd', '4th'];
+// printNumbers = async() => {
+// for await (const number of numbers) {
+// console.log(number);
+// }
+// }
+// printNumbers();
